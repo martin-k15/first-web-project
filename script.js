575a32fff279f4fd86948c1ce2cee7e69eb2601b
martin <kostka.ma@seznam.cz>
2022-07-06 19:28:30 +0200

    Last version of this project..for now.

---
+++ script.js
@@ -0,0 +1,17 @@
+
+/*
+var scrollButton = document.querySelector("#buttonUp");
+
+if (scrollButton) {
+  console.log("go up2");
+  scrollButton.addEventListener("click", event => {
+    console.log("go up222");
+  });
+}
+console.log(scrollButton);
+function scroolUp (){
+  console.log("go up2");
+  window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+};
+
+*/
